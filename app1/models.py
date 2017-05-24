614274128427ef305502008d9b43b51bff1e0cb1
Sala PCZ <a>
2017-05-24 08:56:48 +0000

    css

--- app1/models.py
+++ app1/models.py
@@ -3,17 +3,9 @@ from django.db import models
 class Marka(models.Model):
 	nazwa = models.CharField(max_length=30)
 	kraj_pochodzenia = models.CharField(max_length=30)
-	
-class Samochod(models.Model):
-	marka = models.ForeignKey(Marka)
-	model = models.CharField(max_length=30)
-	moc = models.IntegerField()
-	pojemnosc = models.IntegerField()
-	przebieg = models.IntegerField()
-	paliwo = models.CharField(max_length=20)
-	nadwozie = models.CharField(max_length=20)
-	kolor = models.CharField(max_length=20)
-	cena =  models.IntegerField()
+	def __str__(self):
+	    return self.nazwa
+
 
 class Uzytkownik(models.Model):
 	login = models.CharField(max_length=30, unique=True)
@@ -21,3 +13,21 @@ class Uzytkownik(models.Model):
 	imie = models.CharField(max_length=20)
 	nazwisko = models.CharField(max_length=25)
 	telefon = models.IntegerField()
+	def __str__(self):
+	    return self.login
+
+class Samochod(models.Model):
+	marka = models.ForeignKey(Marka)
+	model = models.CharField(max_length=30)
+	rok = models.IntegerField()
+	moc = models.IntegerField()
+	pojemnosc = models.IntegerField()
+	przebieg = models.IntegerField()
+	paliwo = models.CharField(max_length=20)
+	nadwozie = models.CharField(max_length=20)
+	kolor = models.CharField(max_length=20)
+	cena =  models.IntegerField()
+	uzytkownik = models.ForeignKey(Uzytkownik)
+	def __str__(self):
+	    return self.marka.nazwa + ' ' + self.model
+
